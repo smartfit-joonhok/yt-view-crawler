09233c9920f66100d71307f5aa756bd552f6e9ac
Joonho Kim <pungrue26@gmail.com>
2021-04-15 09:15:21 +0900

    Add scrapper script

---
+++ view_count_scrapper.sh
@@ -0,0 +1,177 @@
+URLS=(
+	"https://www.youtube.com/c/thankyoububu/videos" 
+	"https://www.youtube.com/c/AllblancTV/videos"
+	"https://www.youtube.com/c/%EB%A1%9C%EC%9B%80TV/videos"
+	"https://www.youtube.com/c/%EC%97%84%EB%A7%88TV/videos" #엄마TV
+	"https://www.youtube.com/channel/UC-Ankmj8Hygqha_egG48CmQ/videos" #윤지홈트요가
+	"https://www.youtube.com/channel/UCxWRe2A7LMTEwMBErR4DkgQ/videos" #삐약스핏
+	"https://www.youtube.com/c/JinyKilogram/videos"
+	"https://www.youtube.com/channel/UCRGZCFuFCwp2D_YKByrqRfw/videos" #에이핏
+	"https://www.youtube.com/c/miseowon/videos"
+	"https://www.youtube.com/c/PeltnessDiet/videos"
+	"https://www.youtube.com/c/%EB%B9%B5%EB%8A%90/videos" #빵느
+	"https://www.youtube.com/c/%ED%97%AC%EC%8A%A4%ED%8B%B0%EB%B9%84/videos" #헬스티비
+	"https://www.youtube.com/c/DanoTV/videos"
+	"https://www.youtube.com/channel/UCsyhk1jx2eeafzMSYhQc9Mg/videos" #물리치료사.PT재석
+	"https://www.youtube.com/c/%EA%B0%95%ED%95%98%EB%82%98/videos" #강하나
+	"https://www.youtube.com/channel/UCvCM5nwOg2qsGqVcNhGHFOg/videos" #강하나스트레칭2008
+	"https://www.youtube.com/c/%EC%9A%94%EA%B0%80%EC%86%8C%EB%85%84/videos" #요가소년
+	"https://www.youtube.com/channel/UCcmCbG1-TDwpJwosHt8QP5A/videos" #발레테라핏
+	"https://www.youtube.com/c/%EA%B6%8C%ED%98%8189/videos" #권혁
+	"https://www.youtube.com/c/AngelaChungseumihomegym/videos" #스미홈트
+	"https://www.youtube.com/c/hwangahyoung/videos"
+	"https://www.youtube.com/c/%EC%97%AC%EB%A6%AC%EB%82%98%ED%95%8F/videos" #여리나핏
+    "https://www.youtube.com/c/%EC%9C%A4%EC%8C%A4/videos" #흥둥이
+    "https://www.youtube.com/c/%ED%94%BC%EC%A7%80%EC%98%A4%ED%95%84%EB%9D%BC%EC%86%8C%ED%94%BCPHYSIOPILASOPHY/videos" #자세요정
+    "https://www.youtube.com/c/YoGaEun/videos"
+    "https://www.youtube.com/user/seryeon23/videos" #소피요가
+    "https://www.youtube.com/channel/UC7RHIUypmydTGNnGuxYBbBw/videos" # 요가테라스
+    "https://www.youtube.com/channel/UC4yq3FWEWqMvFNFBsV3gbKQ/videos" #힙으뜸
+    "https://www.youtube.com/c/Fromdew/videos" #듀잇
+    "https://www.youtube.com/c/yafit_videos/videos"
+    "https://www.youtube.com/channel/UC3hRpIQ4x5niJDwjajQSVPg/videos" #핏블리
+    "https://www.youtube.com/c/fitangelsoo/videos" #핏엔젤유튜브
+    "https://www.youtube.com/c/%EB%AC%B4%EB%82%98%ED%99%88%ED%8A%B8MoonaWorkout/videos" #무나홈트
+    "https://www.youtube.com/c/MinsooGo/videos"
+    "https://www.youtube.com/channel/UCzYGcwyrK-RspNMtloM7_CA/videos" #미라클무브 유엘
+    "https://www.youtube.com/c/kimbbomami%EA%B9%80%EB%BD%80%EB%A7%88%EB%AF%B8/videos" #김뽀마미
+    "https://www.youtube.com/channel/UCB7A0Php3CJg4uH4HavVy-w/videos" #고러쉬
+    "https://www.youtube.com/channel/UC35oSnRpFC3E2uAjJj_zmLA/videos" #수빈쌤
+    "https://www.youtube.com/channel/UCvLXPxBnr3Tk9HjKCswy7hw/videos" #온도니의 먹고&운동하고
+    "https://www.youtube.com/c/ShineKimFitness/videos"
+    "https://www.youtube.com/c/%EC%9E%AC%EB%AF%B8%EC%96%B4%ED%8A%B8/videos" #재미어트
+    "https://www.youtube.com/channel/UCiXAtjMLt6epJNJotNMh29A/videos" #콩필라테스
+    "https://www.youtube.com/c/%EB%94%A9%EA%B3%A0%ED%97%AC%EC%8A%A4dingofitness/videos" #딩고헬스
+    "https://www.youtube.com/c/%EC%A0%9C%EC%9D%B4%EC%A0%9C%EC%9D%B4%EC%82%B4%EB%A1%B1%EB%93%9C%ED%95%8F/videos" #제이제이살롱드핏
+    "https://www.youtube.com/c/nancyhomegym/videos"
+    "https://www.youtube.com/c/Hbro%EA%B8%B8%ED%99%98TV/videos" #길환TV
+    "https://www.youtube.com/channel/UCNEqrN9CGcd_RW9Uj9J_FKQ/videos" #운동하는 물리치료사
+    "https://www.youtube.com/channel/UCebO-fOQ_x6OaZ1BoAoFbtw/videos" #FROM LAYA
+    "https://www.youtube.com/channel/UCZC2J9eZXV-9-Xyn7ofFZxA/videos" #Hopisophie
+    "https://www.youtube.com/channel/UCHHGX6n-7T01PV9P2zquXvA/videos" #DeSLun
+    "https://www.youtube.com/channel/UCQ7lGeZ07MQFMAz6gG5On5g/videos" #듀잇 dewit_official
+    "https://www.youtube.com/channel/UC7jfcWMKdtypYEPPtpRboow/videos" #스트레칭조이
+    "https://www.youtube.com/c/HoonYoga/videos"
+    "https://www.youtube.com/user/choichoongsik/videos"
+    "https://www.youtube.com/c/%EB%B0%94%EB%B2%A8%EB%9D%BC%ED%86%A0%EB%A5%B4%ED%99%88%ED%8A%B8%EB%A0%88%EC%9D%B4%EB%8B%9D/videos" #바벨라토르 홈트레이닝
+    "https://www.youtube.com/channel/UC1M6MWxKEX8zvWmYgwra0eg/videos" #민광Ashtanga
+    "https://www.youtube.com/c/DannyJoe/videos"
+    "https://www.youtube.com/c/SNPElife/videos"
+    "https://www.youtube.com/c/DanJeongg/videos"
+    "https://www.youtube.com/c/%EC%9B%8C%EB%84%88%EA%B8%80%EB%9E%A8/videos" #워너글램
+    "https://www.youtube.com/c/BIGSIS/videos"
+)
+MATCH_KEYWORDS=(
+	"챌린지"
+	"효과"
+	"홈트"
+	"홈짐"
+	"엉덩이"
+	"힙업"
+	"허벅지"
+	"종아리"
+	"팔뚝"
+	"복부"
+	"옆구리"
+	"뱃살"
+	"살"
+	"상체"
+	"하체"
+	"코어"
+	"core"
+	"전신"
+	"fullbody"
+	"no equipment"
+	"at home"
+	"유산소"
+	"cardio"
+	"hiit"
+	"복근"
+	"abs"
+	"운동"
+	"workout"
+	"버피"
+	"플랭크"
+	"걷기"
+	"walk"
+	"타바타"
+	"tabata"
+	"요가"
+	"yoga"
+	"스트레칭"
+	"stretch"
+	"필라테스"
+	"pilates"
+	"폭파"
+	"댄스"
+	"급찐급빠"
+	"루틴"
+	"routine"
+	"프로그램"
+	"승모근"
+	"거북목"
+	"교정"
+	"[0-9]분"
+	"min"
+	"하루"
+	"[0-9]일"
+	"[0-9]주"
+)
+REJECT_KEYWORDS=(
+	"브이로그"
+	"VLOG"
+	"일상"
+	"동기"
+	"이벤트"
+	"선물"
+	"라이브"
+	"live"
+	"Q&A"
+	"QnA"
+	"FAQ"
+	"구독자"
+	"소통"
+	"먹방"
+	"리뷰"
+	"인스타"
+	"포즈"
+	"피드백"
+	"상담"
+	"음악"
+	"룩북"
+	"lookbook"
+	"쇼핑"
+	"코디"
+	"팬미팅"
+	"후기"
+	"요리"
+	"밀가루"
+	"식단"
+	"레시피"
+	"recipe"
+	"간식"
+	"식습관"
+	"식욕"
+	"팁"
+	"tip"
+	"습관"
+	"habit"
+	"피부"
+	"생얼"
+	"화장"
+	"#short"
+)
+
+TODAY=$(date +%Y-%m-%d)
+FILE_NAME="${TODAY}.tsv"
+# See more at: https://github.com/ytdl-org/youtube-dl#output-template
+YT_DL_OUTPUT_FORMAT="%(channel)s\t%(title)s\t%(id)s\t%(view_count)s"
+MATCH_TITLE_REGEX=$(IFS=\|; echo "${MATCH_KEYWORDS[*]}")
+REJECT_TITLE_REGEX=$(IFS=\|; echo "${REJECT_KEYWORDS[*]}")
+
+echo "Starting at $(date +"%T")"
+for i in "${URLS[@]}"; do
+	echo "Processing $i"
+    youtube-dl --get-filename --output "$YT_DL_OUTPUT_FORMAT" --match-title "($MATCH_TITLE_REGEX)" --reject-title "($REJECT_TITLE_REGEX)" "$i" >> "$FILE_NAME"
+done
+echo "Ended at $(date +"%T")"
